3fc6eab2f862006f3538e427f721abbe229aa33c
Judith Lütke <judithlachtrup@yahoo.de>
2025-05-31 12:16:13 +0200

    created world and characters, started animation

---
+++ models/movable-object.class.js
@@ -0,0 +1,40 @@
+class MovableObject {
+        x = 120;
+        y = 290;
+        img;
+        height = 150;
+        width = 100;
+        imageCache = {};
+        currentImage = 0;
+        speed = 0.2;
+
+        loadImage(path){
+            this.img = new Image(); 
+            this.img.src = path;
+        }
+
+        /**
+         * 
+         * @param {Array} arr 
+         */
+        loadImages(arr) {
+            arr.forEach((path) => {
+                let img = new Image();
+                img.src = path;
+                this.imageCache[path] = img;
+        });
+    }
+
+        moveRight() {
+            console.log('Moving right');
+        }
+
+
+        moveLeft(){
+            setInterval(() => {
+            this.x -= this.speed;
+            }, 1000 / 60);
+        }
+
+
+}
---
+++ models/world.class.js
@@ -0,0 +1,62 @@
+class World {
+    character = new Character();
+    enemies = [
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 0),
+            
+    ];
+    canvas;
+    ctx;
+    keyboard;
+
+    constructor(canvas, keyboard){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.keyboard = keyboard;
+        this.draw();
+        this.setWorld();
+
+    }
+
+    setWorld() {
+        this.character.world = this;
+    }
+
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addObjectToMap(this.backgroundObjects);
+        this.addToMap(this.character);
+        this.addObjectToMap(this.clouds);
+        this.addObjectToMap(this.enemies);
+        
+
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
+
+    }
+
+    addObjectToMap(objects) {
+        objects.forEach(object => {
+            this.addToMap(object);
+        });
+    }
+
+    addToMap(movableObject) {
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+    }
+
+}
